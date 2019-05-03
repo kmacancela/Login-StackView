ecdd31a225613dc5fbe8dbb4cecd92d4d9a5139f
Karina Macancela <karymacancela@gmail.com>
2019-05-02 21:46:04 -0400

    Initial Commit

---
+++ Login StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login StackView
+//
+//  Created by Karina Macancela on 5/2/19.
+//  Copyright © 2019 Karina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
